c276e6eecbed97d62eaa69a76c0d93be69eebc3a
AyrtonKrizan <ayrtonkrizan@gmail.com>
2019-07-27 20:11:06 -0300

    feat: Create helpers and colors files

---
+++ utils/colors.js
@@ -0,0 +1,7 @@
+export const white = '#fff';
+export const black = '#110c11';
+export const grey = '#808080';
+
+export const darkPrimaryColor =  '#660019';
+export const defaultPrimaryColor = '#800020';
+export const textprimaryColor = '#FFFFFF';
